ef9d992392bd590b3dc6a36d94df41d650b960fc
Pavel Gri <zlibabaika@gmail.com>
2023-02-09 01:14:22 +0400

    skeleton GamePlayView build finished

--- Millionaire/View/GamePlayView/GamePlayView.swift
+++ Millionaire/View/GamePlayView/GamePlayView.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class GamePlayView: UIView {
     
-    let spacingMain: CGFloat = 16
+    let spacingMain: CGFloat = 24
     let offSet: CGFloat = 16
     let spacingBetweenAnswerButton: CGFloat = 14
     
@@ -30,9 +30,7 @@ class GamePlayView: UIView {
     
     private let stackViewBottom = UIStackView()
     
-    let clueFiftyFiftyButton = UIButton()
-    let clueCallFriendButton = UIButton()
-    let clueHelpAudienceButton = UIButton()
+    var clueButtons: [UIButton] = []
     
     /// принимает массив ответов и устанавливает на кнопку
     func configureButton(with answers: [String]) {
@@ -60,14 +58,16 @@ class GamePlayView: UIView {
         setupQuestionNumberLabel()
         setupShoeTableQuestionsButton()
         
+        setupTimerLabel()
         setupQuestionLabel()
         
-        // answer button setup
         setupAnswerButtons()
         setupStackViewContainerAnswers()
         
-        setConstraints()
+        setupClueButtons()
+        setupStackViewBottom()
         
+        setConstraints()
     }
     
     private func setConstraints() {
@@ -89,10 +89,7 @@ class GamePlayView: UIView {
     }
 
     private func setupBackButton() {
-        var imageConfig: UIImage.Configuration
-        imageConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .regular, scale: .large)
-        let image = UIImage(systemName: "arrow.left", withConfiguration: imageConfig)
-        backButton.setImage(image, for: .normal)
+        backButton.setImage(UIImage.getImageSymbolForButton(with: 20, and: "arrow.left"), for: .normal)
         backButton.tintColor = .white
         
         stackViewTop.addArrangedSubview(backButton)
@@ -115,10 +112,7 @@ class GamePlayView: UIView {
     }
     
     private func setupShoeTableQuestionsButton() {
-        var imageConfig: UIImage.Configuration
-        imageConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .regular, scale: .large)
-        let image = UIImage(systemName: "list.bullet.clipboard", withConfiguration: imageConfig)
-        shoeTableQuestionsButton.setImage(image, for: .normal)
+        shoeTableQuestionsButton.setImage(UIImage.getImageSymbolForButton(with: 20, and: "list.bullet.clipboard"), for: .normal)
         shoeTableQuestionsButton.tintColor = .white
         
         stackViewTop.addArrangedSubview(shoeTableQuestionsButton)
@@ -129,17 +123,32 @@ class GamePlayView: UIView {
         ])
     }
     
+    private func setupTimerLabel() {
+        timerLabel.text = "05"
+        timerLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        timerLabel.textColor = .red.withAlphaComponent(0.8)
+        timerLabel.textAlignment = .center
+        timerLabel.backgroundColor = .red.withAlphaComponent(0.3)
+        
+        stackViewMain.addArrangedSubview(timerLabel)
+        
+        NSLayoutConstraint.activate([
+            timerLabel.heightAnchor.constraint(equalToConstant: 50),
+            timerLabel.widthAnchor.constraint(equalToConstant: 200)
+        ])
+        
+        // TODO: нужно делать отдельную вьюшку для этой штуки. Пока тусть так
+    }
+    
     private func setupQuestionLabel() {
-        questionLabel.text = "Some test text! Bla, Bla, Bla"
-        questionLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        questionLabel.text = "Успеет ли команда номер 19 закончить проэкт в срок?"
+        questionLabel.font = UIFont.boldSystemFont(ofSize: 30)
         questionLabel.numberOfLines = 0
         questionLabel.textColor = .white
         questionLabel.textAlignment = .center
         
         stackViewMain.addArrangedSubview(questionLabel)
     }
-    
-    
     
     private func setupStackViewContainerAnswers() {
         stackViewContainerAnswers.axis = .vertical
@@ -150,25 +159,55 @@ class GamePlayView: UIView {
     }
     
     private func setupAnswerButtons() {
-        let number = [1,2,3,4]
-        number.forEach {
-            let button = getButton($0)
+        
+        for _ in 0...3 {
+            let button = getButton()
+            button.layer.cornerRadius = 30
             stackViewContainerAnswers.addArrangedSubview(button)
             
             NSLayoutConstraint.activate([
-                button.heightAnchor.constraint(equalToConstant: 50)
-            ])
+                button.heightAnchor.constraint(equalToConstant: 60)])
+            
+            answerButtons.append(button)
         }
     }
     
-    private func getButton(_ index: Int) -> UIButton {
+    private func getButton() -> UIButton {
         let button = UIButton(type: .system)
-        button.setTitle( "Answer \(index)", for: .normal)
+        button.setTitle( "Answer ", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .blue
         
         return button
     }
+    
+    private func setupStackViewBottom() {
+        stackViewBottom.axis = .horizontal
+        stackViewBottom.spacing = offSet
+        stackViewBottom.distribution = .fillEqually
+
+        stackViewMain.addArrangedSubview(stackViewBottom)
+    }
+    
+    private func setupClueButtons() {
+        for _ in 0...2 {
+           let button = getButton()
+            button.layer.cornerRadius = 30
+            button.setTitle(nil, for: .normal)
+            button.setImage(.getImageSymbolForButton(with: 20, and: "person.fill"), for: .normal)
+            button.tintColor = .white
+            
+            stackViewBottom.addArrangedSubview(button)
+            
+            NSLayoutConstraint.activate([
+                button.heightAnchor.constraint(equalToConstant: 60),
+                button.widthAnchor.constraint(equalToConstant: 100)
+            ])
+            
+            clueButtons.append(button)
+        }
+    }
+    
     
     private func setupStackViewMain() {
         stackViewMain.axis = .vertical
@@ -182,5 +221,19 @@ class GamePlayView: UIView {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Helper
+
+extension UIImage {
+    
+    /// возвращает сконфигурировыный системный UIImage? для кнопки
+    static func getImageSymbolForButton(with size: CGFloat, and systemImage: String) -> UIImage? {
+        var imageConfig: UIImage.Configuration
+        imageConfig = UIImage.SymbolConfiguration(pointSize: size, weight: .regular, scale: .large)
+        let image = UIImage(systemName: systemImage, withConfiguration: imageConfig)
+        
+        return image
     }
 }
