59050632f4479a3a52ccf923d2957834e69ae4ed
Pavel Gri <zlibabaika@gmail.com>
2023-02-09 00:16:23 +0400

    setting top memu

--- Millionaire/View/GamePlayView/GamePlayView.swift
+++ Millionaire/View/GamePlayView/GamePlayView.swift
@@ -11,13 +11,13 @@ class GamePlayView: UIView {
     
     let spacingMain: CGFloat = 16
     let offSet: CGFloat = 16
-    let spacingBetweenAnswerButton: CGFloat = 20
+    let spacingBetweenAnswerButton: CGFloat = 14
     
     private let stackViewMain = UIStackView()
     private let stackViewTop = UIStackView()
     
     let backButton = UIButton()
-    let questionNumber = UILabel()
+    let questionNumberLabel = UILabel()
     let shoeTableQuestionsButton = UIButton()
     
     let timerLabel = UILabel()
@@ -54,7 +54,13 @@ class GamePlayView: UIView {
         self.translatesAutoresizingMaskIntoConstraints = false
         
         setupStackViewMain()
+        
         setupStackViewTop()
+        setupBackButton()
+        setupQuestionNumberLabel()
+        setupShoeTableQuestionsButton()
+        
+        setupQuestionLabel()
         
         // answer button setup
         setupAnswerButtons()
@@ -81,6 +87,59 @@ class GamePlayView: UIView {
         
         stackViewMain.addArrangedSubview(stackViewTop)
     }
+
+    private func setupBackButton() {
+        var imageConfig: UIImage.Configuration
+        imageConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .regular, scale: .large)
+        let image = UIImage(systemName: "arrow.left", withConfiguration: imageConfig)
+        backButton.setImage(image, for: .normal)
+        backButton.tintColor = .white
+        
+        stackViewTop.addArrangedSubview(backButton)
+        
+        NSLayoutConstraint.activate([
+            backButton.heightAnchor.constraint(equalToConstant: 50),
+            backButton.widthAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func setupQuestionNumberLabel() {
+        questionNumberLabel.text = "QUESTION - 1"
+        questionNumberLabel.textColor = .white
+        questionNumberLabel.font = UIFont.boldSystemFont(ofSize: 18)
+        questionNumberLabel.textAlignment = .center
+        
+        stackViewTop.addArrangedSubview(questionNumberLabel)
+        
+        // TODO: подумать как сделать лайбл с разным по стилю текстом
+    }
+    
+    private func setupShoeTableQuestionsButton() {
+        var imageConfig: UIImage.Configuration
+        imageConfig = UIImage.SymbolConfiguration(pointSize: 25, weight: .regular, scale: .large)
+        let image = UIImage(systemName: "list.bullet.clipboard", withConfiguration: imageConfig)
+        shoeTableQuestionsButton.setImage(image, for: .normal)
+        shoeTableQuestionsButton.tintColor = .white
+        
+        stackViewTop.addArrangedSubview(shoeTableQuestionsButton)
+        
+        NSLayoutConstraint.activate([
+            shoeTableQuestionsButton.heightAnchor.constraint(equalToConstant: 50),
+            shoeTableQuestionsButton.widthAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func setupQuestionLabel() {
+        questionLabel.text = "Some test text! Bla, Bla, Bla"
+        questionLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        questionLabel.numberOfLines = 0
+        questionLabel.textColor = .white
+        questionLabel.textAlignment = .center
+        
+        stackViewMain.addArrangedSubview(questionLabel)
+    }
+    
+    
     
     private func setupStackViewContainerAnswers() {
         stackViewContainerAnswers.axis = .vertical
